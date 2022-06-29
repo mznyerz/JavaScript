a6c623c64c3203da544263c2cb0094d993cade38
mznyerz <100621700+mznyerz@users.noreply.github.com>
2022-06-29 15:12:03 +0300

    Update task2+.js

--- HW_3/task2+.js
+++ HW_3/task2+.js
@@ -2,5 +2,18 @@
 // Реализуйте считывание из JSONки из файла task2.json с помощью, например, модуля fs. 
 // Для дальнейшего использования в функции, описанной в задании
 var fs = require('fs');
-var file_readed = fs.readFileSync("D:/js/task2.js", "utf8");
-console.log(file_readed);
+var file_readed = fs.readFileSync("D:/json/task2.json", "utf8");
+// console.log('type file_readed = '+ typeof(file_readed))
+//
+let uniqueName = [];
+let kekw = [];
+const NewUsers = JSON.parse(file_readed).filter(item => {
+    if (!uniqueName.includes(item.name)){
+        uniqueName.push(item.name)
+        console.log(item.name)
+        return kekw.push(item)
+    } else {
+        console.log('duplicate - ', item.name)
+    }
+})
+console.log(kekw)
