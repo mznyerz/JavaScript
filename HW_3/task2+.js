7881b602814016cd4bd79bf28450928a160586ec
Andrey Lapin <andreyqa1998@gmail.com>
2023-01-16 19:06:11 +0300

    Update task2+.js

--- HW_3/task2+.js
+++ HW_3/task2+.js
@@ -2,7 +2,7 @@
 // Реализуйте считывание из JSONки из файла task2.json с помощью, например, модуля fs. 
 // Для дальнейшего использования в функции, описанной в задании
 var fs = require('fs');
-var file_readed = fs.readFileSync("D:/json/task2.json", "utf8");
+var file_readed = fs.readFileSync("./task2.json", "utf8");
 // console.log('type file_readed = '+ typeof(file_readed))
 //
 let uniqueName = [];
